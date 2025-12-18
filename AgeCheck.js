7f841eb20ca383a1be79a7e151ccb8ed38020f53
efpq <insajdded204@gmail.com>
2025-12-18 23:54:04 +0300

    homework

---
+++ AgeCheck.js
@@ -0,0 +1,13 @@
+//Фильтр для "взрослых"(с массивом)
+const ages = [10, 18, 55, 3, 20];
+
+const checkAccess = (age) =>{
+    if (age >= 18){
+        return "Доступ разрешен";
+    }
+    return "Доступ запрещен";
+}
+
+for (const age of ages){
+    console.log(`Возраст ${age}: ${checkAccess(age)}`);
+}
---
+++ converter.js
@@ -0,0 +1,7 @@
+//конвертер валют(доллар/евро - рубли)
+const usdToRub = (usd) => usd * 80;
+
+const euroToRub = (euro) => euro * 94;
+
+console.log(usdToRub(100));
+console.log(euroToRub(500));
